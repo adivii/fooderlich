1fa928227db8a793e597448e89518e80bd0deff9
Adiwijaya Satria Nusantara <adihanyu@gmail.com>
2022-10-27 06:26:20 +0700

    progress 1
    create app state manager
    create app router
    still has errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich/models/models.dart';
+import 'package:provider/provider.dart';
+import 'package:fooderlich/screens/screens.dart';
 
 import 'fooderlich_theme.dart';
 import 'home.dart';
+import 'package:fooderlich/navigation/app_router.dart';
 
-void main() {
-  runApp(const Fooderlich());
+void main() async {
+  final _appStateManager = AppStateManager();
+  await _appStateManager.initializeApp;
+  ChangeNotifierProvider(
+    create: (context) => _appStateManager,
+  );
+  runApp(Fooderlich(appStateManager: AppStateManager));
 }
 
-class Fooderlich extends StatelessWidget {
+class Fooderlich extends StatefulWidget {
   const Fooderlich({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final theme = FooderlichTheme.dark();
     return MaterialApp(title: 'Fooderlich', theme: theme, home: const Home());
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
   }
 }
---
+++ lib/navigation/app_router.dart
@@ -0,0 +1,64 @@
+// Handle Pop Page Belum
+
+import 'package:flutter/cupertino.dart';
+import 'package:fooderlich/models/models.dart';
+import 'package:fooderlich/screens/screens.dart';
+
+class AppRouter extends RouterDelegate
+    with ChangeNotifier, PopNavigatorRouterDelegateMixin {
+  @override
+  final GlobalKey<NavigatorState> navigatorKey;
+
+  final AppStateManager appStateManager;
+  final GroceryManager groceryManager;
+  final ProfileManager profileManager;
+
+  AppRouter({this.appStateManager, this.groceryManager, this.profileManagers})
+      : navigatorKey = GlobalKey<NavigatorState>() {
+    appStateManager.addListener(notifyListeners);
+    groceryManager.addListener(notifyListeners);
+    profileManager.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    appStateManager.removeListener(notifyListeners);
+    groceryManager.removeListener(notifyListeners);
+    profileManager.removeListener(notifyListeners);
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      key: navigatorKey,
+      pages: [
+        // TODO: Add SplashScreen
+        // TODO: Add LoginScreen
+        // TODO: Add OnboardingScreen
+        // TODO: Add Home
+        // TODO: Create new item
+        // TODO: Select GroceryItemScreen
+        // TODO: Add Profile Screen
+        // TODO: Add WebView Screen
+      ],
+    );
+  }
+
+  bool _handlePopPage(Route<dynamic> route, result) {
+    if (!route.didPop(result)) {
+      return false;
+    }
+
+    // TODO: Handle Onboarding and splash
+    // TODO: Handle state when user closes grocery item screen
+    // TODO: Handle state when user closes profile screen
+    // TODO: Handle state when user closes WebView screen
+
+    return true;
+  }
+
+  @override
+  Future<void> setNewRoutePath(configuration) async => null;
+}
